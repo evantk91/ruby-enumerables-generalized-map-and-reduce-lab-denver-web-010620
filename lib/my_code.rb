cdb9b64f883bce52bbd201bd68293e08770c7c81
Evan Greer <etgreer91@gmail.com>
2019-12-16 06:49:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
-# Your Code Here
+def map(array)
+
+  i = 0
+  newArr = []
+
+  while i < array.length do
+    el = array[i]
+    newArr << yield(el)
+   i += 1
+  end
+
+  newArr
+end
+
+def reduce(array, start = 0)
+  i = 0
+  total = start
+  
+  while i < array.length do
+    el = array[i]
+    total = yield(total, el)
+    i += 1
+  end
+  
+  total
+end
